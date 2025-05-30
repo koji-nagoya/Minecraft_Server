4569601a9154213637e1a2462f173c79f86a8323
koji-nagoya <koji@hepl.phys.nagoya-u.ac.jp>
2025-05-30 15:12:11 +0900

    Commit at 2025-05-30 15:12:11

---
+++ backup_to_git.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+set -e
+content_dir="/home/minecrafts/minecraft/bedrock/backups"
+
+set -x
+cd "$content_dir"
+git add -A
+git commit -m "Commit at $(date "+%Y-%m-%d %T")" || true
+git push -f origin main:main
+
